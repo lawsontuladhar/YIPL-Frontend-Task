021005335375a6e0077a83c7fcd6238709397a29
Lawson Tuladhar <132678079+lawsontuladhar@users.noreply.github.com>
2024-06-15 23:14:54 +0545

    News Component Working!

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,6 +1,24 @@
 import NewsCard from "./NewsCard";
+import { useEffect, useState } from "react";
+
+const API_URL =
+  "https://raw.githubusercontent.com/younginnovations/internship-challenges/master/front-end/news_list.json";
 
 function News() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(API_URL)
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setData(data);
+      });
+  }, []);
+  console.log(data);
+
   return (
     <div>
       <section>
@@ -10,10 +28,13 @@ function News() {
           help. We are working at the national and EU levels to advance.
         </p>
       </section>
-      <section>
-        <NewsCard />
-      </section>
-        
+
+      {data && (
+        <section className="news-component">
+          <NewsCard data={data} />
+        </section>
+      )}
+
       <button>View all news</button>
     </div>
   );
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,20 +1,26 @@
-function NewsCard(props) {
+import { useEffect } from "react";
+
+//
+
+function NewsCard({ data }) {
   return (
-    <div>
-      <figure>
-        <img src={props.url} />
-        <h1>{props.title}</h1>
-        <p>{props.description}</p>
-        <button>
-          Learn more
-          <img
-            src="../assets/arrow-right.svg"
-            alt="arrow-icon"
-            width="15px"
-            className="news-container-arrow"
-          />
-        </button>
-      </figure>
+    <div className="news-card-component">
+      {data.news.map((data) => (
+        <figure key={data.id}>
+          <img src={data.image} />
+          <h1>{data.title}</h1>
+          <p>{data.content}</p>
+          <button>
+            Learn more
+            <img
+              src="../assets/arrow-right.svg"
+              alt="arrow-icon"
+              width="15px"
+              className="news-container-arrow"
+            />
+          </button>
+        </figure>
+      ))}
     </div>
   );
 }
